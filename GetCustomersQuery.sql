f8f340292c2772ef42faa6b3e9051509ad8f0af6
Nikolay Aryukov <naryukov@pyrus.com>
2018-01-24 01:07:06 +0300

    formatting

--- GetCustomersQuery.sql
+++ GetCustomersQuery.sql
@@ -1,16 +1,19 @@
-SELECT 	c.Name
-FROM	Customers c
-WHERE	EXISTS (
-			SELECT	*
-			FROM	Purchases p
-			WHERE	p.CustomerId = c.CustomerId
-					AND p.ProductName = 'Молоко'
-		)
-		AND NOT EXISTS (
-			SELECT	*
-			FROM	Purchases p
-			WHERE	p.CustomerId = c.CustomerId
-					AND p.ProductName = 'Сметана'
-					AND MONTH(p.PurchaiseDatetime) = MONTH(GETDATE())
-					AND YEAR(p.PurchaiseDatetime) = YEAR(GETDATE())
-		)
+SELECT
+	c.Name
+FROM
+	Customers c
+WHERE
+	EXISTS (
+		SELECT	*
+		FROM	Purchases p
+		WHERE	p.CustomerId = c.CustomerId
+				AND p.ProductName = 'Молоко'
+	)
+	AND NOT EXISTS (
+		SELECT	*
+		FROM	Purchases p
+		WHERE	p.CustomerId = c.CustomerId
+				AND p.ProductName = 'Сметана'
+				AND MONTH(p.PurchaiseDatetime) = MONTH(GETDATE())
+				AND YEAR(p.PurchaiseDatetime) = YEAR(GETDATE())
+	)
